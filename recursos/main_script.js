4d0b4d50023adfcbd70dcc7c27cbdd918bfb5c94
Adonis Mendoza <donimen1@gmail.com>
2021-12-11 15:52:22 -0400

    Grilla de certificados y stack realizadas

--- recursos/main_script.js
+++ recursos/main_script.js
@@ -1,10 +1,9 @@
 const $formBtn = document.querySelector(".formulario-btn"),
   $form = document.querySelector(".formulario"),
-  $closeFormBtn = document.querySelector(".close-form-btn")
-  $hamburgerBtn = document.querySelector(".hamburger-btn"),
+  $closeFormBtn = document.querySelector(".close-form-btn"),
   $stackItems = document.querySelectorAll(".items"),
   $footer = document.querySelector("footer"),
-  $cabecera = document.querySelector(".cabecera");
+  $cabecera = document.querySelector(".cabecera-container");
 
 const formContact = () => {
   $formBtn.style.display = "none";
@@ -16,10 +15,244 @@ const closeForm = () => {
   $formBtn.style.display = "block"
 }
 
+/* ------animation stack images-------- */
+
+const observersOptions = {
+  threshold: [0.2,0.8]
+}
+
+const animationStackImg = () => {
+
+  const $stackImgs = document.querySelectorAll(".stack-card-container");
+  
+  const callbk1 = (entries) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.style.animationName = "fromLeft";
+        /* console.log(entry) */
+      }
+    })
+  }
+    
+  const observer1 = new IntersectionObserver(callbk1,observersOptions);
+  
+  const callbk2 = (entries) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.style.animationName = "fromRight";
+        /* console.log(entry) */
+      }
+    })
+  }
+    
+  const observer2 = new IntersectionObserver(callbk2,observersOptions);
+  
+  let cont = 0;
+  
+  $stackImgs.forEach(img => {
+  
+    if(innerWidth >= 700 && innerWidth < 1290){
+      observer1.observe(img);
+    }else{
+      if(cont % 2 === 0){
+          observer1.observe(img);
+      }else{
+          observer2.observe(img);
+      }
+      cont++
+    }
+  })
+}
+
+/* -----------------------Form loader----------------------- */
+
+const loader = () => {
+  let loaderContainer = document.createElement("div");
+
+  loaderContainer.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-loader">
+    <line x1="12" y1="2" x2="12" y2="6"/>
+    <line x1="12" y1="18" x2="12" y2="22"/>
+    <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"/>
+    <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"/>
+    <line x1="2" y1="12" x2="6" y2="12"/>
+    <line x1="18" y1="12" x2="22" y2="12"/>
+    <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"/>
+    <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"/>
+  </svg>
+  <br>
+  <span>Enviando...</span>`
+  loaderContainer.style.display = "flex";
+  loaderContainer.style.flexDirection = "column";
+  loaderContainer.classList.add("contact-response-container");
+  loaderContainer.id = "loaderContainer";
+  document.body.insertAdjacentElement("afterbegin",loaderContainer);
+
+  return loaderContainer;
+}
+
+/*----------------ANIMATION LOGO---------------------- */
+
+window.addEventListener("scroll", (e) => {
+
+  if(scrollY > 0){
+    $cabecera.style.animationName = "hideLogo"
+    if($cabecera.style.animationName === "hideLogo"){
+      return ""
+    }
+  }else{
+    $cabecera.style.animationName = "showLogo"
+  }
+
+})
+
+/*------------------ Modal slider ---------------------*/
+
+const testingDimensions = () => {
+  if(innerWidth < 1000){
+    const $sliderContainer = document.querySelector(".slider-img-container");
+  
+    let $modalText = document.querySelector(".tapLeftRight");
+    
+    const watchingSlider = (entries) => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting){
+          $modalText.classList.add("modalText")
+          $modalText.style.animationName = "showModalText";
+        }
+      })
+    }
+    
+    let watchSlider = new IntersectionObserver(watchingSlider,observersOptions)
+    watchSlider.observe($sliderContainer)
+  }
+}
+
+let lines = document.querySelectorAll(".lines div"),
+  techsImg = document.querySelector(".techs-imgs"),
+  techImages = document.querySelectorAll(".techs-imgs img"),
+  linesContainer = document.querySelector(".lines");
+
+const linesAnimation = () => {
+  setInterval(() => {
+    lines.forEach(line => {
+      let random = Math.floor(Math.random()*50),
+      bottomPoints = 100 - random;
+
+      line.style.clipPath = `polygon(0% ${random}%, 100% ${random}%, 100% ${bottomPoints}%, 0% ${bottomPoints}%)`;
+
+    })
+  }, 500);
+}
+
+document.addEventListener("animationend", e => {
+
+  if(e.target.matches(".presentacion")){
+    linesAnimation();
+  }
+
+})
+
+/* -------------------STACK CARDS AND CERTIFICATES---------- */
+
+const $templateCard = document.getElementById("templateCard").content,
+  $templateCertificate = document.getElementById("certificadosCard").content,
+  $fragmentCard = document.createDocumentFragment(),
+  $stackContainer = document.querySelector(".stack-cards"),
+  $certificatesContainer = document.querySelector(".certificados");
+
+const stackData = [
+  {
+    name: "Html",
+    imgUrl: "./imagenes/HTML_Logo.svg",
+    progressPercent: "80%",
+    level: "High"
+  },
+  {
+    name: "Css",
+    imgUrl: "./imagenes/CSS_Logo.svg",
+    progressPercent: "70%",
+    level: "Med"
+  },
+  {
+    name: "JavaScript",
+    imgUrl: "./imagenes/JS_Logo.svg",
+    progressPercent: "72%",
+    level: "Med"
+  },
+  {
+    name: "Git",
+    imgUrl: "./imagenes/git_Logo.svg",
+    progressPercent: "70%",
+    level: "Med"
+  },
+  {
+    name: "React JS",
+    imgUrl: "./imagenes/React_Logo.png",
+    progressPercent: "15%",
+    level: "Basic"
+  }
+]
+
+const certificatesData = [
+  {
+    imgUrl: "./imagenes/Certificado_CSS.png",
+    name: "Css",
+    by: "Educa2"
+  },
+  {
+    imgUrl: "./imagenes/Certificado_js.png",
+    name: "JavaScript",
+    by: "Educa2"
+  }
+]
+
+const creatingCards = () => {
+  stackData.forEach(tech => {
+    let clon = $templateCard.cloneNode(true),
+      $container = clon.querySelector(".stack-card-container"),
+      $img = $container.querySelector(".stack-card-img > img"),
+      $progress = $container.querySelector(".progress-bar"),
+      $level = $container.querySelector(".stack-progress-level p");
+  
+    $img.src = tech.imgUrl;
+    $img.alt = tech.name;
+    $progress.style.width = tech.progressPercent;
+    $level.textContent+= tech.level;
+    $fragmentCard.append($container);
+  })
+
+  $stackContainer.append($fragmentCard);
+
+  /* -------Certificates */
+
+  certificatesData.forEach(cert => {
+    let clon = $templateCertificate.cloneNode(true),
+    $img = clon.querySelector(".certificados-card-img img"),
+    $techName = clon.querySelector(".certificados-card-info-tech-name"),
+    $by = clon.querySelector(".certificados-card-info-tech-by > strong");
+
+    $img.src = cert.imgUrl;
+    $img.alt = `${cert.name} certificate`;
+    $techName.textContent = cert.name;
+    $by.textContent = cert.by;
+    $fragmentCard.append(clon);
+  })
+
+  $certificatesContainer.append($fragmentCard);
+}
+
+/* -----------------EVENTS---------- */
+
+document.addEventListener("DOMContentLoaded",() => {
+  creatingCards();
+  animationStackImg();
+  testingDimensions();
+})
+
 document.addEventListener("click", (e) => {
   if(e.target.matches(`.${$formBtn.className}`)) formContact();
   if(e.target.matches(`.${$closeFormBtn.className}`)) closeForm();
-  if(e.target.matches(`.${$hamburgerBtn.className}`)) menuStack();
 
   const listaItems = document.querySelectorAll(".slider-item");
 
@@ -129,92 +362,7 @@ document.addEventListener("click", (e) => {
   
 })
 
-
-/* ------animation stack images-------- */
-
-const observersOptions = {
-  threshold: [0.2,0.8]
-}
-
-const animationStackImg = () => {
-
-  const $stackImgs = document.querySelectorAll(".stack-card-container");
-  
-  const callbk1 = (entries) => {
-    entries.forEach(entry => {
-      if(entry.isIntersecting){
-        entry.target.style.animationName = "fromLeft";
-        /* console.log(entry) */
-      }
-    })
-  }
-    
-  const observer1 = new IntersectionObserver(callbk1,observersOptions);
-  
-  const callbk2 = (entries) => {
-    entries.forEach(entry => {
-      if(entry.isIntersecting){
-        entry.target.style.animationName = "fromRight";
-        /* console.log(entry) */
-      }
-    })
-  }
-    
-  const observer2 = new IntersectionObserver(callbk2,observersOptions);
-  
-  let cont = 0;
-  
-  $stackImgs.forEach(img => {
-  
-    if(innerWidth >= 700 && innerWidth < 1290){
-      observer1.observe(img);
-    }else{
-      if(cont % 2 === 0){
-          observer1.observe(img);
-      }else{
-          observer2.observe(img);
-      }
-      cont++
-    }
-  })
-}
-
-
-/* ----------Out menu------------ */
-
-document.addEventListener("DOMContentLoaded",() => {
-  creatingCards();
-  animationStackImg();
-})
-
-/* -------Form loader-------------- */
-
-const loader = () => {
-  let loaderContainer = document.createElement("div");
-
-  loaderContainer.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-loader">
-    <line x1="12" y1="2" x2="12" y2="6"/>
-    <line x1="12" y1="18" x2="12" y2="22"/>
-    <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"/>
-    <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"/>
-    <line x1="2" y1="12" x2="6" y2="12"/>
-    <line x1="18" y1="12" x2="22" y2="12"/>
-    <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"/>
-    <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"/>
-  </svg>
-  <br>
-  <span>Enviando...</span>`
-  loaderContainer.style.display = "flex";
-  loaderContainer.style.flexDirection = "column";
-  loaderContainer.classList.add("contact-response-container");
-  loaderContainer.id = "loaderContainer";
-  document.body.insertAdjacentElement("afterbegin",loaderContainer);
-
-  return loaderContainer;
-}
-
-/* FORMULARIO */
+/* -----------------FORMULARIO------------------------ */
 
 const $formContainer = document.querySelector(".formulario-container"),
   $formResponse = document.querySelector(".contact-response-container");
@@ -263,189 +411,3 @@ $formContainer.addEventListener("submit",(e) => {
     },3000)
   })
 })
-
-/* OUT HAMBURGER BTN */
-
-window.addEventListener("scroll", (e) => {
-  if($hamburgerBtn.getBoundingClientRect().top >= ($footer.getBoundingClientRect().top - $hamburgerBtn.clientHeight)){
-    $hamburgerBtn.classList.add("fade-out");
-    $hamburgerBtn.style.pointerEvents = "none";
-  }else{
-    $hamburgerBtn.classList.remove("fade-out"); 
-    $hamburgerBtn.style.pointerEvents = "all";
-  }
-
-  if(scrollY > 0){
-    $cabecera.style.animationName = "hideLogo"
-    if($cabecera.style.animationName === "hideLogo"){
-      return ""
-    }
-  }else{
-    $cabecera.style.animationName = "showLogo"
-  }
-
-})
-
-/* Modal slider */
-
-if(innerWidth < 1000){
-  const $sliderContainer = document.querySelector(".slider-img-container");
-
-  let $modalText = document.querySelector(".tapLeftRight");
-  
-  const watchingSlider = (entries) => {
-    entries.forEach(entry => {
-      if(entry.isIntersecting){
-        $modalText.classList.add("modalText")
-        $modalText.style.animationName = "showModalText";
-      }
-    })
-  }
-  
-  let watchSlider = new IntersectionObserver(watchingSlider,observersOptions)
-  watchSlider.observe($sliderContainer)
-}
-
-let lines = document.querySelectorAll(".lines div"),
-  techsImg = document.querySelector(".techs-imgs"),
-  techImages = document.querySelectorAll(".techs-imgs img"),
-  linesContainer = document.querySelector(".lines");
-
-const linesAnimation = () => {
-  setInterval(() => {
-    lines.forEach(line => {
-      let random = Math.floor(Math.random()*50),
-      bottomPoints = 100 - random;
-
-      line.style.clipPath = `polygon(0% ${random}%, 100% ${random}%, 100% ${bottomPoints}%, 0% ${bottomPoints}%)`;
-
-    })
-  }, 500);
-}
-
-/* const visibleTechImages = () => {
-  let contador = 0,
-  interval = setInterval(() => {
-    techImages[contador].classList.add("visible");
-    techImages[contador].style.animationName = "lightning";
-    techImages[contador].style.animationIterationCount = "1";
-    techImages[contador].style.animationDuration = "1.5s";
-    techImages[contador].style.animationTimingFunction = "ease";
-    techImages[contador].style.animationFillMode = "forwards";
-    contador++;
-    if(contador === techImages.length){
-      console.log("Limpio");
-      rotate3d();
-      clearInterval(interval)
-    } 
-  }, 1000);
-}
-
-const rotate3d = () => {
-  techsImg.style.animationName = "rotate3d";
-} */
-
-/* document.addEventListener("animationend",e => {
-  if(e.target.matches(".proob")){
-    visibleTechImages();
-  }
-}) */
-
-document.addEventListener("animationend", e => {
-  /* console.log(e) */
-
-  if(e.target.matches(".presentacion")){
-    linesAnimation();
-  }
-
-})
-
-const $templateCard = document.getElementById("templateCard").content,
-  $templateCertificate = document.getElementById("certificadosCard").content,
-  $fragmentCard = document.createDocumentFragment(),
-  $stackContainer = document.querySelector(".stack_contenedor"),
-  $certificatesContainer = document.querySelector(".certificados-contenedor");
-
-const stackData = [
-  {
-    name: "Html",
-    imgUrl: "./imagenes/HTML_Logo.svg",
-    progressPercent: "80%",
-    level: "High"
-  },
-  {
-    name: "Css",
-    imgUrl: "./imagenes/CSS_Logo.svg",
-    progressPercent: "80%",
-    level: "Med"
-  },
-  {
-    name: "JavaScript",
-    imgUrl: "./imagenes/JS_Logo.svg",
-    progressPercent: "85%",
-    level: "Med"
-  },
-  {
-    name: "Git",
-    imgUrl: "./imagenes/git_Logo.svg",
-    progressPercent: "75%",
-    level: "Med"
-  },
-  {
-    name: "React JS",
-    imgUrl: "./imagenes/React_Logo.png",
-    progressPercent: "15%",
-    level: "Basic"
-  }
-]
-
-const certificatesData = [
-  {
-    imgUrl: "./imagenes/Certificado_CSS.png",
-    name: "Css",
-    by: "Educa2"
-  },
-  {
-    imgUrl: "./imagenes/Certificado_js.png",
-    name: "JavaScript",
-    by: "Educa2"
-  }
-]
-
-const creatingCards = () => {
-  stackData.forEach(tech => {
-    let clon = $templateCard.cloneNode(true),
-      $container = clon.querySelector(".stack-card-container"),
-      $img = $container.querySelector(".stack-card-img > img"),
-      $progress = $container.querySelector(".progress-bar"),
-      $level = $container.querySelector(".stack-progress-level p");
-  
-    $img.src = tech.imgUrl;
-    $img.alt = tech.name;
-    $progress.style.width = tech.progressPercent;
-    $level.textContent+= tech.level;
-    $fragmentCard.append($container);
-  })
-
-  $stackContainer.append($fragmentCard);
-
-  /* -------Certificates */
-
-  certificatesData.forEach(cert => {
-    let clon = $templateCertificate.cloneNode(true),
-    $img = clon.querySelector(".certificados-card-img img"),
-    $techName = clon.querySelector(".certificados-card-info-tech-name"),
-    $by = clon.querySelector(".certificados-card-info-tech-by > strong");
-
-    $img.src = cert.imgUrl;
-    $img.alt = `${cert.name} certificate`;
-    $techName.textContent = cert.name;
-    $by.textContent = cert.by;
-    $fragmentCard.append(clon);
-  })
-
-  $certificatesContainer.append($fragmentCard);
-}
-
-/* --------------------------- */
-
